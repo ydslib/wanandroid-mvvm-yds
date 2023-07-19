d06107c698050c1f020769cd7c807ea7ecb6fe65
dongsheng ye <yedongsheng@codemao.cn>
2023-07-19 11:41:53 +0800

    新增新模块

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -6,7 +6,7 @@ object Deps{
     const val versionCode = 1
     const val versionName = "1.0"
 
-    const val appcompat = "1.2.0"
+    const val appcompat = "1.3.1"
     const val core_ktx = "1.6.0"
     const val constraintlayout = "2.0.4"
     const val test_junit = "4.13"
@@ -28,4 +28,6 @@ object Deps{
     const val refreshLayoutVersion = "2.0.6"
 
     const val lottieVersion = "6.0.0"
+    const val BaseRecyclerViewAdapterHelper = "3.0.10"
+    const val glideVersion = "4.15.1"
 }
---
+++ eyepetizer/src/main/java/com/yds/eyepetizer/EyepetizerActivity.kt
@@ -0,0 +1,11 @@
+package com.yds.eyepetizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EyepetizerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eyepetizer)
+    }
+}
